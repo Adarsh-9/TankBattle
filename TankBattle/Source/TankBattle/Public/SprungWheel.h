87639821602570b821a59e4cb089fee6b9c21c7b
Adarsh S <siddabattula.adarsh1111@gmail.com>
2020-04-15 17:19:22 +0530

    Suspension System Added

---
+++ TankBattle/Source/TankBattle/Public/SprungWheel.h
@@ -0,0 +1,36 @@
+// Copyright : Adarsh.S 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SprungWheel.generated.h"
+class UPhysicsConstraintComponent;
+class USphereComponent;
+UCLASS()
+class TANKBATTLE_API ASprungWheel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASprungWheel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USphereComponent* Axle = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		USphereComponent* Wheel = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UPhysicsConstraintComponent* MassWheelConstraint = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UPhysicsConstraintComponent* AxleWheelConstraint = nullptr;
+	void SetupConstraint();
+};
